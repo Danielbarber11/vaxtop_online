135a55aab8da033d047cc5a43e0feef4fe4875a4
Danielbarber11 <Danielbarber1246@gmail.com>
2025-11-01 15:13:07 +0200

    חיטה הקטנה: הצג AuthScreen עם גוגל, אפל, יחטובי login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AuthScreen from './screens/AuthScreen';
 
 const App = () => {
   return (
-    <div style={{padding: '20px', fontSize: '24px'}}>
-      אתר vaxtop עולה!
+    <div style={{minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <AuthScreen />
     </div>
   );
 };
