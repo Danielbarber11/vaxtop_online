72c3db304d557fcaad3c75b9aa73c0f23a7b04bb
Danielbarber11 <Danielbarber1246@gmail.com>
2025-11-01 15:38:26 +0200

    Wrap AuthScreen in GoogleAuthProvider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { GoogleAuthProvider } from './context/GoogleAuthContext';
 import AuthScreen from './screens/AuthScreen';
 
 const App = () => {
   return (
-    <div style={{minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      <AuthScreen />
-    </div>
+    <GoogleAuthProvider>
+      <div style={{minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <AuthScreen />
+      </div>
+    </GoogleAuthProvider>
   );
 };
 
